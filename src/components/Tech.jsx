d6a1431a9eec8eca4a62f846e9082106a52b9034
Purnima <purnimavats6789@gmail.com>
2024-09-25 20:10:47 +0530

    removed 3d models

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { techStack } from "../constants";
 
 // Importing styles and data from external files
 import { styles } from "../styles";
@@ -21,13 +22,35 @@ const Tech = () => {
                 </h2>
             </motion.div>
 
-            <div className="flex flex-row flex-wrap justify-center gap-10 mt-2">
+            {/* <div className="flex flex-row flex-wrap justify-center gap-10 mt-2">
                 {technologies.map((technology) => (
                     <div className="w-28 h-28" key={technology.name}>
                         <BallCanvas icon={technology.icon} />
                     </div>
                 ))}
-            </div>
+            </div> */}
+            <motion.div className="flex flex-wrap mt-8 justify-between ">
+                {techStack.map((el, index) => (
+                    <motion.div
+                        initial="hidden"
+                        whileInView={"visible"}
+                        variants={{
+                            visible: {
+                                y: 0,
+                                opacity: 1,
+                                transition: {
+                                    type: "spring",
+                                },
+                            },
+                            hidden: { opacity: 1, y: 80 },
+                        }}
+                        className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40"
+                    >
+                        <img alt="" src={el.link} className="w-12" />
+                        <h4 className="text-md ml-4 text-black">{el.name}</h4>
+                    </motion.div>
+                ))}
+            </motion.div>
         </>
     );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -25,7 +25,7 @@ import {
     apple,
     carepulse,
     idyll_logo,
-    discord_clone
+    discord_clone,
 } from "../assets";
 
 export const navLinks = [
@@ -130,7 +130,7 @@ const experiences = [
         date: "September 2024",
         points: [
             "Developed a responsive and user-friendly website for Idyll, a dating app startup, using HTML, Tailwind CSS, and JavaScript in just 15 days.",
-            "Improved overall performance and enhanced user experience with modern, clean design."
+            "Improved overall performance and enhanced user experience with modern, clean design.",
         ],
     },
     // {
@@ -255,7 +255,8 @@ const projects = [
     // },
     {
         name: "Discord Clone",
-        description: "Built a real-time messaging platform with 1:1 video calls, message attachments, and role-based management. Integrated WebSocket messaging via Socket.io, Prisma with MySQL (Planetscale), and authentication using Clerk. The responsive UI, designed with TailwindCSS and ShadcnUI, supports light/dark modes for all devices.",
+        description:
+            "Built a real-time messaging platform with 1:1 video calls, message attachments, and role-based management. Integrated WebSocket messaging via Socket.io, Prisma with MySQL (Planetscale), and authentication using Clerk. The responsive UI, designed with TailwindCSS and ShadcnUI, supports light/dark modes for all devices.",
         tags: [
             {
                 name: "Next.js",
@@ -339,9 +340,116 @@ const projects = [
             },
         ],
         image: apple,
-        source_code_link: "https://github.com/Purnima-Vats/apple-iPhone15-clone",
+        source_code_link:
+            "https://github.com/Purnima-Vats/apple-iPhone15-clone",
         live_link: "https://appoll.netlify.app",
     },
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+const techStack = [
+    // {
+    //     name: "C Language",
+    //     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg",
+    // },
+    {
+        name: "TypeScript",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    },
+    {
+        name: "Express",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+    },
+
+    {
+        name: "NodeJS",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    },
+    {
+        name: "Postman",
+        link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+    },
+
+    // {
+    //     name: "Dart",
+    //     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg",
+    // },
+    // {
+    //     name: "Flutter",
+    //     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-plain.svg",
+    // },
+    // {
+    //     name: "React Native",
+    //     link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png",
+    // },
+    {
+        name: "HTML",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    },
+    {
+        name: "CSS",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+    },
+    // {
+    //     name: "Bootstrap",
+    //     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+    // },
+    // {
+    //     name: "Sass",
+    //     link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+    // },
+
+    {
+        name: "Javascript",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    },
+    {
+        name: "Redux",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+    },
+    {
+        name: "Tailwind CSS",
+        link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    },
+    {
+        name: "React",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    },
+    {
+        name: "Next",
+        link: "https://www.svgrepo.com/show/354113/nextjs-icon.svg"
+    },
+    {
+        name: "MySQL",
+        link: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Database-mysql.svg/1200px-Database-mysql.svg.png",
+    },
+    {
+        name: "Mongo DB",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
+    },
+    {
+        name: "Clerk",
+        link: "https://imgix.cosmicjs.com/9d8bc340-e63d-11ee-b074-b5c8fe3ef189-clerk.webp?w=1200&auto=format,compression",
+    },
+    // {
+    //     name: "Heroku",
+    //     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg",
+    // },
+    // { name: "Azure", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg" },
+    {
+        name: "Git",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+    },
+    // { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" },
+    // {
+    //     name: "Firebase",
+    //     link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+    // },
+
+    // { name: "Bash", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" },
+    // { name: "Vim", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg" },
+
+    // { name: "VS Code", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
+    // {name: "", link: "https://repository-images.githubusercontent.com/59065830/b62be480-45d2-11ea-9989-803db0f9c44d"}
+];
+
+export { services, technologies, experiences, testimonials, projects, techStack };
